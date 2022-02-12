0cf96fbd0f115c5a38373b308e6cdcaefa1b9d1d
tarikly <tarikly+github@gmail.com>
2022-02-12 13:09:09 -0300

    test: bug added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('Hello World!');
+   res.send('Hello Worl!');
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
